64c8a52a7265aec0c05ebb812458d3bf74aaf1dc
stefanallenspach <stefan.allenspach.sync@outlook.com>
2018-09-04 18:49:29 +0200

    erste logik eingebaut

--- app/src/main/java/com/example/asp/luiscalc/MainActivity.java
+++ app/src/main/java/com/example/asp/luiscalc/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.asp.luiscalc;
 
-        import android.graphics.Color;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.TextView;
 
         import java.util.ArrayList;
         import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Button> buttonArrayList= new ArrayList<>();
+    private ArrayList<Button> buttonArrayList= new ArrayList<>();
+    private String calc;
+    private int resalt;
+    private TextView textViewTimer;
+    private TextView textViewCalculation;
+    private TextView textViewScore;
+
 
 
     @Override
@@ -24,26 +30,43 @@ public class MainActivity extends AppCompatActivity {
         buttonArrayList.add((Button) findViewById(R.id.button2));
         buttonArrayList.add((Button) findViewById(R.id.button3));
 
+        textViewTimer = findViewById(R.id.textViewTimer);
+        textViewCalculation = findViewById(R.id.textViewCalculation);
+        textViewScore = findViewById(R.id.textViewScore);
 
 
-        start(null);
+        newGame(null);
 
     }
-    public void start(View view){
-        for(int i = 0; i <4; i++){
+    public void newGame(View view){
+
+
+
+        for(int i = 0; i
+                <4; i++){
             Button button = buttonArrayList.get(i);
-            button.setText(Integer.toString(generateResalt(10)));
+            button.setText(Integer.toString(generateRandom(10)));
         }
 
     }
 
-    public int generateResalt(int limit){
+    public int generateRandom(int limit){
         return new Random().nextInt(limit);
     }
 
     public void click(View view){
         Log.i("info","Es sollte was passiern");
         Log.i("info", view.getTag().toString());
+    }
+
+    public void generateCalc(){
+        int a = generateRandom(10);
+        int b = generateRandom(10);
+        resalt = a+b;
+        calc = a + " + " + b;
+
+
+
     }
 
 
